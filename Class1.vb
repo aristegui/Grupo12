6dafd692b0701777abb390eb695ebd488d6a2cc8
aristegui <55279820+aristegui@users.noreply.github.com>
2020-03-24 20:45:08 +0100

    LAB5
    Lab5

---
+++ Class1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Net.NetworkCredentials
+Imports System.Net.Mail
+Class Class1
+    Public Function enviarEmail(email As String, url As String
+                                ) As Boolean
+        Try
+            Dim msg = New MailMessage()
+            msg.To.Add(New MailAddress(email, "HADS"))
+            msg.From = New MailAddress("xpersona001@ikasle.ehu.eus", "HADS")
+            msg.Subject = "Confirmación de registro"
+            msg.Body = url
+
+            msg.IsBodyHtml = True
+            Dim client = New SmtpClient()
+            client.UseDefaultCredentials = False
+            client.Credentials = New System.Net.NetworkCredential("839403", "RM3D2kt6")
+            client.Port = 587
+            client.Host = "smtp.ehu.eus"
+            client.DeliveryMethod = SmtpDeliveryMethod.Network
+            client.EnableSsl = True
+            client.Send(msg)
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+    Public Function enviarPassNuevo(email As String) As Boolean
+        Try
+            Dim msg = New MailMessage()
+            msg.To.Add(New MailAddress(email, "HADS"))
+            msg.From = New MailAddress("xpersona001@ikasle.ehu.eus", "HADS")
+            msg.Subject = "Recuperación del password"
+            msg.Body = "Tú codigo para el cambio de contraseña es el siguiente: 123456"
+            msg.IsBodyHtml = True
+            Dim client = New SmtpClient()
+            client.UseDefaultCredentials = False
+            client.Credentials = New System.Net.NetworkCredential("839403", "RM3D2kt6")
+            client.Port = 587
+            client.Host = "smtp.ehu.eus"
+            client.DeliveryMethod = SmtpDeliveryMethod.Network
+            client.EnableSsl = True
+            client.Send(msg)
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+End Class
---
+++ iniciadosesion.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="iniciadosesion.aspx.vb" Inherits="Lab2.iniciadosesion" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            <br />
+            <br />
+            <br />
+            Se ha confirmado su registro, pulse para ir al inicio de sesión<br />
+            <asp:Button ID="Button1" runat="server" Text="Log In" />
+        </div>
+    </form>
+</body>
+</html>
